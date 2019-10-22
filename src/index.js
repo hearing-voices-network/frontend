e5cc40114ab093a5760d540698028857b8ef31ed
jaderyan <jadeelizabethryan@outlook.com>
2019-10-22 15:06:14 +0100

    add main styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 import "normalize.css";
+import "./styles/main.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
