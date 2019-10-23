2fb2c7d5d61cee68032a149a2405a907330863bc
jaderyan <jadeelizabethryan@outlook.com>
2019-10-23 10:49:59 +0100

    set prop types for accordian children

--- src/components/Accordian/Accordian.tsx
+++ src/components/Accordian/Accordian.tsx
@@ -5,7 +5,7 @@ import cx from "classnames";
 import "./Accordian.scss";
 
 interface IProps {
-  children: any;
+  children: JSX.Element | JSX.Element[];
   title: string;
   subtitle?: string;
   green?: boolean;
@@ -30,7 +30,7 @@ const Accordian: React.FunctionComponent<IProps> = ({
         )}
         onClick={() => toggleAccordian(!open)}
         aria-expanded={open}
-        aria-controls="accordian--content"
+        aria-controls="accordian-content"
         id="accordian-header"
       >
         <div className="accordian--title-container flex-container flex-container--no-padding flex-container--no-space flex-container--align-center">
@@ -47,7 +47,7 @@ const Accordian: React.FunctionComponent<IProps> = ({
       </button>
       {open && (
         <div
-          className="accordian-content"
+          className="accordian--content"
           id="accordian-content"
           aria-labelledby="accordian-header"
         >
