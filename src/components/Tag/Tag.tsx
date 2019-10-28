88ebfc8f096e2bb99850f5dfb498a805185ed07f
jaderyan <jadeelizabethryan@outlook.com>
2019-10-28 15:06:20 +0000

    search tag styling

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,14 +1,24 @@
 import React, { FunctionComponent } from "react";
+import cx from "classnames";
 
 import "./Tag.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface IProps {
   text: string;
+  search?: boolean;
 }
 
-const Tag: FunctionComponent<IProps> = ({ text }) => (
-  <div className="tag">
-    <span>{text}</span>
+const Tag: FunctionComponent<IProps> = ({ text, search }) => (
+  <div
+    className={cx("tag", {
+      "tag--search": search
+    })}
+  >
+    <span>
+      {text}
+      {search && <FontAwesomeIcon icon="times" className="tag--remove" />}
+    </span>
   </div>
 );
 
