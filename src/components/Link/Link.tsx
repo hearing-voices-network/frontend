190e831749c99fe0ca8b4b59299e5d91d2023d60
jaderyan <jadeelizabethryan@outlook.com>
2019-10-28 14:31:38 +0000

    add grey

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from "react";
+import cx from "classnames";
 
 import "./Link.scss";
 
@@ -6,10 +7,16 @@ interface IProps {
   text: string;
   href: string;
   size: "small" | "medium" | "large";
+  grey?: boolean;
 }
 
-const Link: FunctionComponent<IProps> = ({ text, href, size }) => (
-  <a href={href} className={`link link--${size}`}>
+const Link: FunctionComponent<IProps> = ({ text, href, size, grey }) => (
+  <a
+    href={href}
+    className={cx(`link link--${size}`, {
+      "link--grey": grey
+    })}
+  >
     {text}
   </a>
 );
