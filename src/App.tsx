3cb092f0c09863e500ea0dd51f027a5ff0b6ac4a
jaderyan <jadeelizabethryan@outlook.com>
2019-11-05 14:41:31 +0000

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Home from "./views/Home";
 import NotFound from "./views/NotFound";
+import Header from "./components/Header";
 
 library.add(fas);
 
 const App: FunctionComponent = () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" component={Home} exact={true} />
       <Route component={NotFound} />
