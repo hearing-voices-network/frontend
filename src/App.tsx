b91e77d7c80f8050a7557fb335091fdbf2f454f5
jaderyan <jadeelizabethryan@outlook.com>
2019-11-06 10:49:37 +0000

    display cookie banner if cookies have not been accepted

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import React, { FunctionComponent } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "mobx-react";
+
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import "./styles/main.scss";
 
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./styles/main.scss";
 
 import Home from "./views/Home";
 import NotFound from "./views/NotFound";
 import Header from "./components/Header";
+import CookiesStore from "./stores/cookiesStore";
 
 library.add(fas);
 
+const cookieStore = new CookiesStore();
+
 const App: FunctionComponent = () => (
-  <Router>
-    <Header />
-    <Switch>
-      <Route path="/" component={Home} exact={true} />
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
+  <Provider cookieStore={cookieStore}>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;
--- src/components/Cookies/Cookies.tsx
+++ src/components/Cookies/Cookies.tsx
@@ -1,54 +1,65 @@
 import React, { FunctionComponent } from "react";
 import cx from "classnames";
+import { inject, observer } from "mobx-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import CookiesStore from "../../stores/cookiesStore";
 
 import "./Cookies.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface IProps {
   loggedIn?: boolean;
+  cookieStore?: CookiesStore;
 }
 
-const Cookies: FunctionComponent<IProps> = ({ loggedIn }) => (
-  <div
-    className={cx(
-      "flex-container flex-container--mobile-no-padding flex-container--align-center cookies-banner",
-      {
-        "cookies-banner--grey": loggedIn
-      }
-    )}
-  >
-    <div className="flex-col--mobile--12 mobile-show">
-      <div className="flex-container flex-container--mobile-no-padding flex-container--align-center">
-        <div className="flex-col--mobile--6">
-          <span className="cookies-banner--header">Cookies</span>
-        </div>
-        <div className="flex-col--mobile--6 cookies-banner--close">
-          <span>
-            Close
-            <FontAwesomeIcon
-              icon="times"
-              className="cookies-banner--close--icon"
-            />
-          </span>
+const Cookies: FunctionComponent<IProps> = ({ loggedIn, cookieStore }) => {
+  if (!cookieStore || cookieStore.cookiesAccepted) return null;
+
+  return (
+    <div
+      className={cx(
+        "flex-container flex-container--mobile-no-padding flex-container--align-center cookies-banner",
+        {
+          "cookies-banner--grey": loggedIn
+        }
+      )}
+    >
+      <div className="flex-col--mobile--12 mobile-show">
+        <div className="flex-container flex-container--mobile-no-padding flex-container--align-center">
+          <div className="flex-col--mobile--6">
+            <span className="cookies-banner--header">Cookies</span>
+          </div>
+          <div className="flex-col--mobile--6 cookies-banner--close">
+            <span role="button" onClick={() => cookieStore.acceptCookies()}>
+              Close
+              <FontAwesomeIcon
+                icon="times"
+                className="cookies-banner--close--icon"
+              />
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="flex-col--2 flex-col--tablet--3 flex-col--mobile--7 mobile-hide">
-      <span className="cookies-banner--header">Cookies</span>
-    </div>
+      <div className="flex-col--2 flex-col--tablet--3 flex-col--mobile--7 mobile-hide">
+        <span className="cookies-banner--header">Cookies</span>
+      </div>
 
-    <div className="flex-col--8 flex-col--tablet--6 flex-col--mobile--12 cookies-banner--content">
-      Lorem ipsum dolor sit amet, adipiscing elit. Vivamus ultricies tincidunt
-      congue. Lorem ipsum dolor sit amet, adipiscing elit. Vivamus ultricies
-      tincidunt congue.
+      <div className="flex-col--8 flex-col--tablet--6 flex-col--mobile--12 cookies-banner--content">
+        Lorem ipsum dolor sit amet, adipiscing elit. Vivamus ultricies tincidunt
+        congue. Lorem ipsum dolor sit amet, adipiscing elit. Vivamus ultricies
+        tincidunt congue.
+      </div>
+      <div className="flex-col--2 flex-col--tablet--3 flex-col--mobile--5 mobile-hide cookies-banner--close">
+        <span>
+          Close
+          <FontAwesomeIcon
+            icon="times"
+            className="cookies-banner--close--icon"
+          />
+        </span>
+      </div>
     </div>
-    <div className="flex-col--2 flex-col--tablet--3 flex-col--mobile--5 mobile-hide cookies-banner--close">
-      <span>
-        Close
-        <FontAwesomeIcon icon="times" className="cookies-banner--close--icon" />
-      </span>
-    </div>
-  </div>
-);
+  );
+};
 
-export default Cookies;
+export default inject("cookieStore")(observer(Cookies));
