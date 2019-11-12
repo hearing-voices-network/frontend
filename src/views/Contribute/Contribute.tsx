9ae2d4590677888b291791f7f66dd1970fd62b36
jaderyan <jadeelizabethryan@outlook.com>
2019-11-12 10:17:53 +0000

    make styles reusable

--- src/views/Contribute/Contribute.tsx
+++ src/views/Contribute/Contribute.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import "./Contribute.scss";
+import Breadcrumb from "../../components/Breadcrumb";
+import { cms } from "../../utils/cms";
+import Button from "../../components/Button";
+import NavigationBlock from "../../components/NavigationBlock";
 
 const Contribute = () => (
-  <div className="flex-container flex-container--no-padding flex-container--center contribute">
-    Contribute
-  </div>
+  <Fragment>
+    <div className="flex-container flex-container--no-padding flex-container--center contribute">
+      <Breadcrumb
+        crumbs={[{ text: "Home", url: "/" }, { text: "Contribute", url: "" }]}
+      />
+      <div className="flex-col--12">
+        <h1 className="contribute--subtitle">{cms("global.tagline")}</h1>
+      </div>
+
+      <div className="flex-col--8 flex-col--tablet-large--12">
+        <p className="contribute--about">{cms("global.about")}</p>
+      </div>
+    </div>
+    <NavigationBlock
+      leftButton={
+        <Button
+          text={cms("contribute.login-cta")}
+          twoCol={true}
+          onClick={() => console.log("login")}
+        />
+      }
+      leftTitle={cms("contribute.login")}
+      leftDescription={cms("contribute.disclaimer")}
+      rightButton={
+        <Button
+          text={cms("contribute.share-cta")}
+          twoCol={true}
+          onClick={() => console.log("login")}
+        />
+      }
+      rightTitle={cms("contribute.share")}
+      rightDescription={cms("contribute.disclaimer")}
+    />
+  </Fragment>
 );
 
 export default Contribute;
