210b7c9f756171bc3750339bc231b661a57e812c
jaderyan <jadeelizabethryan@outlook.com>
2019-11-12 13:53:52 +0000

    login header and description

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,7 +1,15 @@
 import React, { FunctionComponent } from "react";
 
 import "./Login.scss";
+import { cms } from "../../utils/cms";
 
-const Login: FunctionComponent = () => <div>LOGIN</div>;
+const Login: FunctionComponent = () => (
+  <div className="flex-container flex-container--no-padding flex-container--center login">
+    <div className="flex-col--12 login--inner">
+      <h1 className="login--title">{cms("login.title")}</h1>
+      <p className="login--description">{cms("login.description")}</p>
+    </div>
+  </div>
+);
 
 export default Login;
