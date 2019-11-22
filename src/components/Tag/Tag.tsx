ba9b33ad2ecba931ee51c70da2a1c32c097133ed
jaderyan <jadeelizabethryan@outlook.com>
2019-11-22 15:08:21 +0000

    show selected tags

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -8,15 +8,26 @@ interface IProps {
   search?: boolean;
   story?: boolean;
   className?: string;
+  onClick?: any;
+  selected?: boolean;
 }
 
-const Tag: FunctionComponent<IProps> = ({ text, search, story, className }) => (
+const Tag: FunctionComponent<IProps> = ({
+  text,
+  search,
+  story,
+  className,
+  onClick,
+  selected
+}) => (
   <div
     className={cx("tag", {
       "tag--search": search,
       "tag--story": story,
+      "tag--selected": selected,
       [`${className}`]: className
     })}
+    onClick={onClick}
   >
     <span>{text}</span>
   </div>
