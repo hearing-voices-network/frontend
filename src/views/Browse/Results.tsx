8fe7ccc8366a563f2d40337cf6045b7eaeaea7a5
jaderyan <jadeelizabethryan@outlook.com>
2019-11-25 13:39:24 +0000

    add loading states

--- src/views/Browse/Results.tsx
+++ src/views/Browse/Results.tsx
@@ -5,6 +5,7 @@ import "./Results.scss";
 
 import ExperienceStore from "../../stores/experienceStore";
 import NoExperiences from "./NoExperiences";
+import Loading from "../../components/Loading";
 
 interface IProps {
   experienceStore?: ExperienceStore;
@@ -13,11 +14,21 @@ interface IProps {
 const Results: FunctionComponent<IProps> = ({ experienceStore }) => {
   if (!experienceStore) return null;
 
-  const { experiences } = experienceStore;
+  const { experiences, experiencesLoading } = experienceStore;
 
   return (
     <div className="flex-container flex-container--no-padding flex-container--center results">
-      {!experiences.length && <NoExperiences />}
+      {!experiences.length && !experiencesLoading && <NoExperiences />}
+
+      {experiencesLoading && <Loading input="selection" />}
+
+      {!experiencesLoading && !!experiences.length && (
+        <div className="flex-col--12">
+          <p className="results--count">{`${experiences.length} ${
+            experiences.length > 1 ? "stories" : "story"
+          }`}</p>
+        </div>
+      )}
     </div>
   );
 };
