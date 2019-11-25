075667cdf69f259c2a203fda01747f652919c09e
jaderyan <jadeelizabethryan@outlook.com>
2019-11-25 14:39:30 +0000

    add storys

--- src/views/Browse/Results.tsx
+++ src/views/Browse/Results.tsx
@@ -1,11 +1,14 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, Fragment } from "react";
 import { observer, inject } from "mobx-react";
 
 import "./Results.scss";
 
+import { IStory } from "../../utils/types";
 import ExperienceStore from "../../stores/experienceStore";
+
 import NoExperiences from "./NoExperiences";
 import Loading from "../../components/Loading";
+import Story from "../../components/Cards/Story";
 
 interface IProps {
   experienceStore?: ExperienceStore;
@@ -17,17 +20,26 @@ const Results: FunctionComponent<IProps> = ({ experienceStore }) => {
   const { experiences, experiencesLoading } = experienceStore;
 
   return (
-    <div className="flex-container flex-container--no-padding flex-container--center results">
+    <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify results">
       {!experiences.length && !experiencesLoading && <NoExperiences />}
 
       {experiencesLoading && <Loading input="selection" />}
 
       {!experiencesLoading && !!experiences.length && (
-        <div className="flex-col--12">
-          <p className="results--count">{`${experiences.length} ${
-            experiences.length > 1 ? "stories" : "story"
-          }`}</p>
-        </div>
+        <Fragment>
+          <div className="flex-col--12">
+            <p className="results--count">{`${experiences.length} ${
+              experiences.length > 1 ? "stories" : "story"
+            }`}</p>
+          </div>
+          <div className="flex-col--12">
+            <div className="flex-container flex-container--no-padding">
+              {experiences.map((experience: IStory) => (
+                <Story story={experience.content} tags={experience.tags} />
+              ))}
+            </div>
+          </div>
+        </Fragment>
       )}
     </div>
   );
