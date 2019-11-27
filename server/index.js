b50f9d28d02bee9e111013e4b758c91fb97d227c
jaderyan <jadeelizabethryan@outlook.com>
2019-11-27 17:21:33 +0000

    serve react app and proxy requests

--- server/index.js
+++ server/index.js
@@ -2,14 +2,25 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const serve = require("koa-static");
 const path = require("path");
+const proxy = require("koa-proxies");
 
 const FRONTEND_APP_BUILD_PATH = path.resolve(__dirname, "../build");
 
 const app = new Koa();
 const router = new Router();
 
+app.use(
+  proxy("/api", {
+    target: "http://localhost/v1",
+    rewrite: path => path.replace(/^\/api/, ""),
+    logs: true
+  })
+);
+
+app.use(router.routes()).use(router.allowedMethods());
+
 app.use(serve(FRONTEND_APP_BUILD_PATH));
-app.use(router.routes());
+
 app.use(
   async (ctx, next) =>
     await serve(FRONTEND_APP_BUILD_PATH)(
