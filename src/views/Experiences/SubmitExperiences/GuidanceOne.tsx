f8f3678da235522f645de35d6e14525f3544c947
jaderyan <jadeelizabethryan@outlook.com>
2019-11-29 14:54:43 +0000

    guidance step two

--- src/views/Experiences/SubmitExperiences/GuidanceOne.tsx
+++ src/views/Experiences/SubmitExperiences/GuidanceOne.tsx
@@ -8,8 +8,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const GuidanceOne = () => (
   <div className="flex-col--8 flex-col--tablet-large--12 guidance">
-    <ReactSVG src={GuidanceIcon} />
-    <h1 className="guidance--title">{cms("writing-guidance.title")}</h1>
+    <ReactSVG src={GuidanceIcon} className="guidance--image" />
+    <h1 className="guidance--title">
+      {cms("writing-guidance.guidance-one-title")}
+    </h1>
     <span className="guidance--step">1/2</span>
     <p className="guidance--content">
       {cms("writing-guidance.guidance-one-1")}
--- src/views/Experiences/SubmitExperiences/GuidanceTwo.tsx
+++ src/views/Experiences/SubmitExperiences/GuidanceTwo.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { cms } from "../../../utils/cms";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const GuidanceTwo = () => <div className="flex-col--12">Guidance Two</div>;
+const GuidanceTwo = () => (
+  <div className="flex-col--8 flex-col--tablet-large--12 guidance">
+    <h1 className="guidance--title">
+      {cms("writing-guidance.guidance-two-title")}
+    </h1>
+    <span className="guidance--step">2/2</span>
+    <p className="guidance--content">
+      {cms("writing-guidance.guidance-two-1")}
+    </p>
+
+    <p className="guidance--content">
+      {cms("writing-guidance.guidance-two-2")}
+    </p>
+
+    <FontAwesomeIcon icon="circle" className="guidance--icon" />
+    <FontAwesomeIcon
+      icon="circle"
+      className="guidance--icon guidance--icon--active"
+    />
+  </div>
+);
 
 export default GuidanceTwo;
