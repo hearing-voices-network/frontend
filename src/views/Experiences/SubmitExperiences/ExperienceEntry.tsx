276184a7a2ab9a0032e33f205ab8522ba4900d18
jaderyan <jadeelizabethryan@outlook.com>
2019-12-03 13:33:59 +0000

    add footer

--- src/views/Experiences/SubmitExperiences/ExperienceEntry.tsx
+++ src/views/Experiences/SubmitExperiences/ExperienceEntry.tsx
@@ -1,12 +1,14 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, Fragment } from "react";
 import { withRouter, RouteComponentProps } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { inject, observer } from "mobx-react";
+import cx from "classnames";
+
 import { cms } from "../../../utils/cms";
 import Editor from "../../../components/Editor";
-import { inject } from "mobx-react";
-import { observer } from "mobx-react-lite";
 import ContributionStore from "../../../stores/contributionStore";
-import cx from "classnames";
+import Footer from "../../../components/Footer";
+import Button from "../../../components/Button";
 
 interface IProps extends RouteComponentProps {
   contributionStore?: ContributionStore;
@@ -21,45 +23,71 @@ const ExperienceEntry: FunctionComponent<IProps> = ({
   if (!contributionStore) return null;
 
   return (
-    <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify submit-experiences">
-      <div className="flex-col--12">
-        <button
-          onClick={() => history.goBack()}
-          className="my-experiences--back"
-        >
-          <FontAwesomeIcon icon="chevron-left" /> Back
-        </button>
-      </div>
-      <div className="flex-col--12 submission--header">
-        <h1 className="submission--title">{cms("submission.title")}</h1>
-        <p className="submission--date">{cms("submission.date")}</p>
-      </div>
+    <Fragment>
+      <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify submit-experiences">
+        <div className="flex-col--12">
+          <button
+            onClick={() => history.goBack()}
+            className="my-experiences--back"
+          >
+            <FontAwesomeIcon icon="chevron-left" /> Back
+          </button>
+        </div>
+        <div className="flex-col--12 submission--header">
+          <h1 className="submission--title">{cms("submission.title")}</h1>
+          <p className="submission--date">{cms("submission.date")}</p>
+        </div>
 
-      <div className="flex-col--12 submission--editor">
-        <Editor
-          text={contributionStore.contribution}
-          onChange={contributionStore.onContributionChange}
-        />
-      </div>
-      <div className="flex-col--12 submission--word-count">
-        {contributionStore.wordCount > MAX_WORD_COUNT && (
-          <p className="submission--word-count--max">
-            You have reached the word count limit!
+        <div className="flex-col--12 submission--editor">
+          <Editor
+            text={contributionStore.contribution}
+            onChange={contributionStore.onContributionChange}
+          />
+        </div>
+        <div className="flex-col--12 submission--word-count">
+          {contributionStore.wordCount > MAX_WORD_COUNT && (
+            <p className="submission--word-count--max">
+              You have reached the word count limit!
+            </p>
+          )}
+          <p></p>
+          <p
+            className={cx("submission--word-count--count", {
+              "submission--word-count--count-max":
+                contributionStore.wordCount > MAX_WORD_COUNT
+            })}
+          >
+            Word count
+            <span className="submission--word-count--count-max">{` ${contributionStore.wordCount} `}</span>
+            {`/ ${MAX_WORD_COUNT}`}
           </p>
-        )}
-        <p></p>
-        <p
-          className={cx("submission--word-count--count", {
-            "submission--word-count--count-max":
-              contributionStore.wordCount > MAX_WORD_COUNT
-          })}
-        >
-          Word count
-          <span className="submission--word-count--count-max">{` ${contributionStore.wordCount} `}</span>
-          {`/ ${MAX_WORD_COUNT}`}
-        </p>
+        </div>
       </div>
-    </div>
+      <Footer green={true}>
+        <div className="flex-container flex-container--center flex-container--justify experience-entry--footer">
+          <div className="flex-col--8 flex-col--tablet-large--10 guidance--footer--content">
+            <h5 className="experience-entry--footer--title">
+              {cms("submission.footer.title")}
+            </h5>
+            <p className="experience-entry--footer--description">
+              {cms("submission.footer.step-1-about")}
+            </p>
+            <Button
+              text="Next Step"
+              onClick={() => contributionStore.increaseStep("contribution")}
+            />
+          </div>
+          <div className="flex-col--8 flex-col--tablet-large--12 guidance--footer--content">
+            <button
+              onClick={() => contributionStore.skipGuidance()}
+              className="experience-entry--footer--cancel"
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </Footer>
+    </Fragment>
   );
 };
 
