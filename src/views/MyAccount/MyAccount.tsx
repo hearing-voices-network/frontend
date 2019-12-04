0d6e648e91e8905aa564733d99fef1248f6667ee
jaderyan <jadeelizabethryan@outlook.com>
2019-12-04 14:46:46 +0000

    add back button

--- src/views/MyAccount/MyAccount.tsx
+++ src/views/MyAccount/MyAccount.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import Layout from "../../components/Layout";
 
-const MyAccount = () => (
+import "./MyAccount.scss";
+import { observer } from "mobx-react";
+import { withRouter, RouteComponentProps } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const MyAccount: FunctionComponent<RouteComponentProps> = ({ history }) => (
   <Layout>
-    <div className="flex-container">my account</div>
+    <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify my-account">
+      <div className="flex-col--12">
+        <div className="flex-col--12">
+          <button onClick={() => history.goBack()} className="my-account--back">
+            <FontAwesomeIcon icon="chevron-left" /> Back
+          </button>
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
-export default MyAccount;
+export default withRouter(observer(MyAccount));
