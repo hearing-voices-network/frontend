594126ca71295809efada0175185479c976e63af
jaderyan <jadeelizabethryan@outlook.com>
2019-12-05 14:25:26 +0000

    update privacy policy styles

--- src/views/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/views/PrivacyPolicy/PrivacyPolicy.tsx
@@ -1,23 +1,28 @@
 import React, { FunctionComponent } from "react";
-import Breadcrumb from "../../components/Breadcrumb";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { observer } from "mobx-react";
+import { withRouter, RouteComponentProps } from "react-router";
 
 import { cms } from "../../utils/cms";
 
 import "./PrivacyPolicy.scss";
 import Layout from "../../components/Layout";
 
-const PrivacyPolicy: FunctionComponent = () => (
+const PrivacyPolicy: FunctionComponent<RouteComponentProps> = ({ history }) => (
   <Layout>
-    <div className="flex-container flex-container--no-padding flex-container--center privacy-policy">
+    <div className="flex-container flex-container--no-padding flex-container--justify flex-container--center privacy-policy">
       <div className="flex-col--12">
-        <Breadcrumb
-          crumbs={[
-            { text: "Home", url: "/" },
-            { text: "Privacy Policy", url: "" }
-          ]}
-        />
+        <div className="flex-col--12">
+          <button
+            onClick={() => history.goBack()}
+            className="privacy-policy--back"
+          >
+            <FontAwesomeIcon icon="chevron-left" /> Back
+          </button>
+        </div>
       </div>
-      <div className="flex-col--8 flex-col--tablet-large--12">
+      <div className="flex-col--8 flex-col--tablet-large--12 privacy-policy--content--container">
+        <h1 className="privacy-policy--title">Privacy Policy</h1>
         <p
           className="privacy-policy--content"
           dangerouslySetInnerHTML={{ __html: cms("privacy.content") }}
@@ -27,4 +32,4 @@ const PrivacyPolicy: FunctionComponent = () => (
   </Layout>
 );
 
-export default PrivacyPolicy;
+export default withRouter(observer(PrivacyPolicy));
