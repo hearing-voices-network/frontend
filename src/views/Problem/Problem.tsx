142cfe539c3df1f00cc4fc141cc7e07473a4c58a
jaderyan <jadeelizabethryan@outlook.com>
2019-12-05 11:11:08 +0000

    add problem page

--- src/views/Problem/Problem.tsx
+++ src/views/Problem/Problem.tsx
@@ -4,6 +4,9 @@ import { observer } from "mobx-react";
 import { withRouter, RouteComponentProps } from "react-router";
 
 import Layout from "../../components/Layout";
+import { cms } from "../../utils/cms";
+import Footer from "../../components/Footer";
+import Link from "../../components/Link";
 
 const Problem: FunctionComponent<RouteComponentProps> = ({ history }) => (
   <Layout>
@@ -13,7 +16,36 @@ const Problem: FunctionComponent<RouteComponentProps> = ({ history }) => (
           <FontAwesomeIcon icon="chevron-left" /> Back
         </button>
       </div>
+
+      <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify my-account--container">
+        <div className="flex-col--8 flex-col--tablet-large--12">
+          <h1 className="my-account--title">{cms("problem.title")}</h1>
+          <p className="update-email--about">{cms("problem.about")}</p>
+        </div>
+
+        <div className="flex-col--6 flex-col--tablet-large--11 contact">
+          <p>{cms("home.footer.contact")}</p>
+          <a className="contact--email" href={`mailto:${cms("global.email")}`}>
+            {cms("global.email")}
+          </a>
+        </div>
+      </div>
     </div>
+    <Footer grey={true}>
+      <div className="flex-container flex-container--center flex-container--justify my-account--footer">
+        <div className="flex-col--8 flex-col--tablet-large--10 my-account--footer--content">
+          <Link
+            text="Privacy Policy"
+            href="/privacy-policy"
+            size="medium"
+            grey={true}
+          />
+          <p className="my-account--footer--about">
+            {cms("my-account.footer.about")}
+          </p>
+        </div>
+      </div>
+    </Footer>
   </Layout>
 );
 
