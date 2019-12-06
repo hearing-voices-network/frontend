abdb9250e746e67f697e99924406873610f0d06f
Jade <jade@ayup.agency>
2019-12-06 10:20:33 +0000

    Feature/logout (#63)
    * add logout to dashboard
    
    * add logout to header
    
    * hook up logout

--- src/components/Header/UserHeader.tsx
+++ src/components/Header/UserHeader.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from "react";
 import ReactSVG from "react-svg";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { observer, inject } from "mobx-react";
 
 import Cookies from "../Cookies";
 import Logo from "../../assets/logo/logo_white.svg";
@@ -10,91 +12,102 @@ import Account from "../../assets/icons/account-light.svg";
 import Dashboard from "../../assets/icons/dashboard-light.svg";
 
 import "./Header.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import UserStore from "../../stores/userStore";
 
-const UserHeader: FunctionComponent = () => (
-  <header>
-    <Cookies />
-    <div className="user-header">
-      <div className="flex-container flex-container--no-padding flex-container--center">
-        <div className="flex-col--12 header--logo-container">
-          <Link to="/" className="header--link">
-            <img
-              src={HVN}
-              className="header--logo--square"
-              alt="Hearing Voices Network logo"
-            />
-            <ReactSVG src={Logo} className="header--logo" wrapper="span" />
-          </Link>
-        </div>
+interface IProps {
+  userStore?: UserStore;
+}
 
-        {/* Mobile Links  */}
-        <div className="mobile-show header--links--outer">
-          <div className="flex-container flex-container--no-padding header--links">
+const UserHeader: FunctionComponent<IProps> = ({ userStore }) => {
+  if (!userStore) return null;
+
+  return (
+    <header>
+      <Cookies />
+      <div className="user-header">
+        <div className="flex-container flex-container--no-padding flex-container--center">
+          <div className="flex-col--12 header--logo-container">
+            <Link to="/" className="header--link">
+              <img
+                src={HVN}
+                className="header--logo--square"
+                alt="Hearing Voices Network logo"
+              />
+              <ReactSVG src={Logo} className="header--logo" wrapper="span" />
+            </Link>
+          </div>
+
+          {/* Mobile Links  */}
+          <div className="mobile-show header--links--outer">
+            <div className="flex-container flex-container--no-padding header--links">
+              <span className="user-header--link">
+                <Link className="header--link user-header--link" to="/about">
+                  About
+                  <ReactSVG
+                    src={Question}
+                    wrapper="span"
+                    className="header--icon"
+                  />
+                </Link>
+              </span>
+              <span className="user-header--link">
+                <Link className="header--link" to="/dashboard">
+                  Dashboard
+                  <ReactSVG
+                    src={Dashboard}
+                    wrapper="span"
+                    className="header--icon"
+                  />
+                </Link>
+              </span>
+              <span className="user-header--link">
+                <Link className="header--link" to="/account">
+                  My account
+                  <ReactSVG
+                    src={Account}
+                    wrapper="span"
+                    className="header--icon"
+                  />
+                </Link>
+              </span>
+            </div>
+          </div>
+
+          {/* Desktop Links */}
+
+          <div className="flex-container flex-container--no-padding header--links mobile-hide">
             <span className="user-header--link">
-              <Link className="header--link user-header--link" to="/about">
-                About
-                <ReactSVG
-                  src={Question}
-                  wrapper="span"
-                  className="header--icon"
-                />
+              <FontAwesomeIcon
+                icon="chevron-left"
+                className="user-header--chevron"
+              />
+              <Link className="header--link user-header--link" to="/">
+                Go back to Connecting Voices
               </Link>
             </span>
             <span className="user-header--link">
-              <Link className="header--link" to="/dashboard">
-                Dashboard
-                <ReactSVG
-                  src={Dashboard}
-                  wrapper="span"
-                  className="header--icon"
-                />
+              <Link className="header--link user-header--link" to="/browse">
+                View other's stories
               </Link>
             </span>
             <span className="user-header--link">
               <Link className="header--link" to="/account">
                 My account
-                <ReactSVG
-                  src={Account}
-                  wrapper="span"
-                  className="header--icon"
-                />
               </Link>
+            </span>
+            <span className="user-header--link">
+              <button
+                className="header--link"
+                onClick={() => userStore.logOut()}
+              >
+                Log out
+              </button>
             </span>
           </div>
         </div>
-
-        {/* Desktop Links */}
-
-        <div className="flex-container flex-container--no-padding header--links mobile-hide">
-          <span className="user-header--link">
-            <FontAwesomeIcon
-              icon="chevron-left"
-              className="user-header--chevron"
-            />
-            <Link className="header--link user-header--link" to="/">
-              Go back to Connecting Voices
-            </Link>
-          </span>
-          <span className="user-header--link">
-            <Link className="header--link user-header--link" to="/browse">
-              View other's stories
-            </Link>
-          </span>
-          <span className="user-header--link">
-            <Link className="header--link" to="/account">
-              My account
-            </Link>
-          </span>
-          <span className="user-header--link">
-            <Link className="header--link" to="/logout">
-              Log out
-            </Link>
-          </span>
-        </div>
       </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default UserHeader;
+export default inject("userStore")(observer(UserHeader));
