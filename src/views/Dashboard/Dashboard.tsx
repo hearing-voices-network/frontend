a4db7bf5e75ad640c5c92bbf8f0fc1887330d69e
jaderyan <jadeelizabethryan@outlook.com>
2019-12-06 10:14:06 +0000

    add logout to dashboard

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ import {
   withRouter,
   RouteComponentProps
 } from "react-router-dom";
-import { observer } from "mobx-react";
+import { observer, inject } from "mobx-react";
 
 import "./Dashboard.scss";
 
@@ -15,97 +15,111 @@ import NavigationBlock from "../../components/NavigationBlock";
 import Button from "../../components/Button";
 import Privacy from "../../components/Cards/Privacy";
 import Footer from "../../components/Footer";
-import Link from "../../components/Link";
+import UserStore from "../../stores/userStore";
 
-const Dashboard: FunctionComponent<RouteComponentProps> = ({ history }) => (
-  <Layout>
-    <div className="flex-container flex-container--no-padding flex-container--center dashboard">
-      <div className="flex-col--8 flex-col--tablet-large--12">
-        <p className="dashboard--about">{cms("dashboard.about")}</p>
-      </div>
-      <div className="flex-col--12">
-        <NavigationBlock
-          leftButton={
-            <Button
-              text="View my experiences"
-              twoCol={true}
-              onClick={() =>
-                history.push({
-                  pathname: "/my-experiences"
-                })
-              }
-            />
-          }
-          mobileLeftButton={
-            <Button
-              text="View my experiences"
-              onClick={() =>
-                history.push({
-                  pathname: "/my-experiences"
-                })
-              }
-            />
-          }
-          leftTitle={cms("dashboard.experiences")}
-          leftDescription=""
-          rightButton={
-            <Button
-              text="Submit new experience"
-              twoCol={true}
-              onClick={() => history.push({ pathname: "/submit-experience" })}
-            />
-          }
-          mobileRightButton={
-            <Button
-              text="Submit new experience"
-              twoCol={true}
-              onClick={() => history.push({ pathname: "/submit-experience" })}
-            />
-          }
-          rightTitle={cms("dashboard.submit")}
-          rightDescription=""
-          mobileDescription={cms("dashboard.description")}
-        />
-      </div>
-      <div className="flex-col--mobile--12 mobile-show">
-        <Privacy />
-      </div>
-    </div>
-    {/* Mobile Footer */}
-    <Footer purple={true} className="mobile-show">
-      <div className="flex-container flex-container--center dashboard--footer">
+interface IProps extends RouteComponentProps {
+  userStore?: UserStore;
+}
+
+const Dashboard: FunctionComponent<IProps> = ({ history, userStore }) => {
+  if (!userStore) return null;
+
+  return (
+    <Layout>
+      <div className="flex-container flex-container--no-padding flex-container--center dashboard">
         <div className="flex-col--8 flex-col--tablet-large--12">
-          <Link size="medium" text={cms("dashboard.footer.title")} href="/" />
-          <p className="dashboard--footer--description">
-            {cms("dashboard.footer.description")}
-          </p>
+          <p className="dashboard--about">{cms("dashboard.about")}</p>
+        </div>
+        <div className="flex-col--12">
+          <NavigationBlock
+            leftButton={
+              <Button
+                text="View my experiences"
+                twoCol={true}
+                onClick={() =>
+                  history.push({
+                    pathname: "/my-experiences"
+                  })
+                }
+              />
+            }
+            mobileLeftButton={
+              <Button
+                text="View my experiences"
+                onClick={() =>
+                  history.push({
+                    pathname: "/my-experiences"
+                  })
+                }
+              />
+            }
+            leftTitle={cms("dashboard.experiences")}
+            leftDescription=""
+            rightButton={
+              <Button
+                text="Submit new experience"
+                twoCol={true}
+                onClick={() => history.push({ pathname: "/submit-experience" })}
+              />
+            }
+            mobileRightButton={
+              <Button
+                text="Submit new experience"
+                twoCol={true}
+                onClick={() => history.push({ pathname: "/submit-experience" })}
+              />
+            }
+            rightTitle={cms("dashboard.submit")}
+            rightDescription=""
+            mobileDescription={cms("dashboard.description")}
+          />
+        </div>
+        <div className="flex-col--mobile--12 mobile-show">
+          <Privacy />
         </div>
       </div>
-    </Footer>
 
-    {/* Desktop Footer */}
-    <Footer grey={true} className="mobile-hide">
-      <div className="flex-container flex-container--center dashboard--footer">
-        <div className="flex-col--8 flex-col--tablet-large--12">
-          <h3 className="dashboard--footer--title">
-            {cms("privacy-notice.title")}
-          </h3>
-          <p className="dashboard--footer--privacy-about">
-            {cms("privacy-notice.about")}
-          </p>
+      {/* Mobile Footer */}
+      <Footer purple={true} className="mobile-show">
+        <div className="flex-container flex-container--center dashboard--footer">
+          <div className="flex-col--8 flex-col--tablet-large--12">
+            <button
+              className="dashboard--footer--logout"
+              onClick={() => userStore.logOut()}
+            >
+              {cms("dashboard.footer.title")}
+            </button>
+            <p className="dashboard--footer--description">
+              {cms("dashboard.footer.description")}
+            </p>
+          </div>
         </div>
+      </Footer>
 
-        <div className="flex-col--8  flex-col--tablet--8 flex-col--tablet-large--8 dashboard--footer--link--container">
-          <RouteLink to="/privacy-policy" className="dashboard--footer--link">
-            {cms("privacy-notice.policy")}
-          </RouteLink>
-          <RouteLink to="/withdraw" className="dashboard--footer--link">
-            {cms("privacy-notice.withdraw")}
-          </RouteLink>
+      {/* Desktop Footer */}
+      <Footer grey={true} className="mobile-hide">
+        <div className="flex-container flex-container--center dashboard--footer">
+          <div className="flex-col--8 flex-col--tablet-large--12">
+            <h3 className="dashboard--footer--title">
+              {cms("privacy-notice.title")}
+            </h3>
+            <p className="dashboard--footer--privacy-about">
+              {cms("privacy-notice.about")}
+            </p>
+          </div>
+
+          <div className="flex-col--8  flex-col--tablet--8 flex-col--tablet-large--8 dashboard--footer--link--container">
+            <RouteLink to="/privacy-policy" className="dashboard--footer--link">
+              {cms("privacy-notice.policy")}
+            </RouteLink>
+            <RouteLink to="/withdraw" className="dashboard--footer--link">
+              {cms("privacy-notice.withdraw")}
+            </RouteLink>
+          </div>
         </div>
-      </div>
-    </Footer>
-  </Layout>
-);
+      </Footer>
+    </Layout>
+  );
+};
 
-export default withRouter(observer(Dashboard));
+export default inject("userStore")(withRouter(observer(Dashboard)));
