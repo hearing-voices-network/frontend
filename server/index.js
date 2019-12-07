5de0d5a2782be2faf2b6201d8144e43ee694f7ee
Matthew Inamdar <matt@goldspecdigital.com>
2019-12-07 20:44:11 +0000

    Using different proxy package

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require("koa-bodyparser");
 const session = require("koa-session");
 const serve = require("koa-static");
 const path = require("path");
-const proxy = require("koa-proxies");
+const proxy = require("koa-proxy");
 const axios = require("axios");
 require("dotenv").config();
 
@@ -100,12 +100,12 @@ app
       ] = `Bearer ${ctx.session.access_token}`;
     }
 
-    return proxy("/api", {
-      target: `${process.env.API_URL}/v1`,
-      rewrite: path => {
-        return path.replace(/^\/api/, "");
-      },
-      logs: true
+    return proxy({
+      host: process.env.API_URL,
+      match: /^\/api/,
+      map: path => {
+        return path.replace(/^\/api/, "/v1");
+      }
     })(ctx, next);
   })
   // Serve the static files in the build directory.
