addb45fa33763e1854f9dea8619331f6d6d695b3
jaderyan <jadeelizabethryan@outlook.com>
2019-12-11 09:01:00 +0000

    add entry preciew under day

--- src/views/Experiences/MyExperiences/Experiences.tsx
+++ src/views/Experiences/MyExperiences/Experiences.tsx
@@ -1,9 +1,12 @@
-import React, { FunctionComponent, Fragment } from "react";
-import { IStory } from "../../../utils/types";
-import { format } from "date-fns";
-import { Dictionary } from "lodash";
+import React, { FunctionComponent } from "react";
+import ReactMarkdown from "react-markdown";
 import map from "lodash/map";
+import { Dictionary } from "lodash";
+
+import { IStory, ITag } from "../../../utils/types";
 import Accordian from "../../../components/Accordian";
+
+import "../../../styles/markdown.scss";
 
 interface IProps {
   experiences: Dictionary<IStory[]>;
@@ -18,7 +21,24 @@ const Day: FunctionComponent<IDayProps> = ({ date, experiences }) => (
   <Accordian title={date} green={true} subtitle="1 private, 1 in public">
     <div>
       {experiences.map((story: IStory) => (
-        <p>{story.excerpt}</p>
+        <div className="flex-container flex-container--no-padding entry-summary">
+          <div className="flex-col--9">
+            <ReactMarkdown className="markdown" source={story.excerpt} />
+            {!!story.tags.length && (
+              <div className="entry-summary--tags">
+                {story.tags.map((tag: ITag, i: number) => (
+                  <span>{`${tag.name}${
+                    i < story.tags.length - 1 ? ", " : ""
+                  }`}</span>
+                ))}
+              </div>
+            )}
+          </div>
+
+          <div className="flex-col--3">
+            <p>{story.status}</p>
+          </div>
+        </div>
       ))}
     </div>
   </Accordian>
