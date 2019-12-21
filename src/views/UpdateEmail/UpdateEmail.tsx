14d460e2c8ec7f2f507b6edfe8c63090bd40530b
jaderyan <jadeelizabethryan@outlook.com>
2019-12-21 15:09:09 +0000

    add verifcation to hint

--- src/views/UpdateEmail/UpdateEmail.tsx
+++ src/views/UpdateEmail/UpdateEmail.tsx
@@ -3,9 +3,11 @@ import { observer, inject } from "mobx-react";
 import { withRouter, RouteComponentProps } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link as RouterLink } from "react-router-dom";
+import ReactSVG from "react-svg";
 
 import Layout from "../../components/Layout";
 import { cms } from "../../utils/cms";
+import Question from "../../assets/icons/question-circle.svg";
 
 import "./UpdateEmail.scss";
 
@@ -25,55 +27,67 @@ const UpdateEmail: FunctionComponent<IProps> = ({ history, userStore }) => {
   return (
     <Layout>
       <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify update-email">
-        <div className="flex-col--1 my-account--back--container">
+        <div className="flex-col--12 my-account--back--container">
           <button onClick={() => history.goBack()} className="my-account--back">
             <FontAwesomeIcon icon="chevron-left" /> Back
           </button>
         </div>
-
-        <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify my-account--container">
-          <div className="flex-col--8 flex-col--tablet-large--12">
-            <h1 className="update-email--title">{cms("update-email.title")}</h1>
-            <p className="update-email--about">{cms("update-email.about")}</p>
-          </div>
-
-          <form className="flex-col--8 flex-col--tablet-large--10 flex-col--mobile--11">
-            <Input
-              id="new-email"
-              label="New email"
-              placeholder="user@email.com"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                userStore.handleChange(e.target.value, "newEmail")
-              }
-            />
-
-            <div className="update-email--form">
-              {userStore.changeEmailSuccess && (
-                <p className="update-email--confirmation">
-                  Email verification sent.
-                </p>
-              )}
-
-              {!!userStore.changeEmailErrors.length && (
-                <Fragment>
-                  {userStore.changeEmailErrors.map(error => (
-                    <p className="update-email--error">{error}</p>
-                  ))}
-                </Fragment>
-              )}
-
-              <p>Send verifcation email</p>
-              <Button
-                purple={true}
-                text="Save"
-                type="submit"
-                onClick={(e: React.FormEvent<HTMLButtonElement>) => {
-                  e.preventDefault();
-                  userStore.resetEmail();
-                }}
-              />
+        <div className="flex-col--12">
+          <div className="flex-container flex-container--no-padding flex-container--center flex-container--justify my-account--container">
+            <div className="flex-col--8 flex-col--tablet-large--12">
+              <h1 className="update-email--title">
+                {cms("update-email.title")}
+              </h1>
+              <p className="update-email--about">{cms("update-email.about")}</p>
             </div>
-          </form>
+
+            <form className="flex-col--8 flex-col--tablet-large--10 flex-col--mobile--11">
+              <Input
+                id="new-email"
+                label="New email"
+                placeholder="user@email.com"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  userStore.handleChange(e.target.value, "newEmail")
+                }
+              />
+
+              <div className="update-email--form">
+                {userStore.changeEmailSuccess && (
+                  <p className="update-email--confirmation">
+                    Email verification sent.
+                  </p>
+                )}
+
+                {userStore.changeEmailErrors &&
+                  !!userStore.changeEmailErrors.length && (
+                    <Fragment>
+                      {userStore.changeEmailErrors.map(error => (
+                        <p className="update-email--error">{error}</p>
+                      ))}
+                    </Fragment>
+                  )}
+
+                <div className="flex-container flex-container--justify flex-container--no-padding">
+                  <div className="flex-col--mobile--11 flex-col--tablet--6 flex-col--standard--8 flex-col--tablet-large--8 flex-col--8">
+                    <div className="register--hint">
+                      <ReactSVG src={Question} />
+                      <span> Send verifcation email</span>
+                    </div>
+                  </div>
+                </div>
+
+                <Button
+                  purple={true}
+                  text="Save"
+                  type="submit"
+                  onClick={(e: React.FormEvent<HTMLButtonElement>) => {
+                    e.preventDefault();
+                    userStore.resetEmail();
+                  }}
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
 
