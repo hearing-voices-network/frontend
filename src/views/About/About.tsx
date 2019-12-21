2d1a26040ed6a235943594189cf6018b8596f84c
Jade <jade@ayup.agency>
2019-12-21 14:01:08 +0000

    Feature/about page styyles (#81)
    * dont show breadcrumbs if logged in
    
    * different footer if logged in

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,57 +1,93 @@
 import React, { FunctionComponent } from "react";
 import { withRouter, RouteComponentProps } from "react-router";
+import { inject } from "mobx-react";
+import cx from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./About.scss";
 import Breadcrumb from "../../components/Breadcrumb";
-
 import { cms } from "../../utils/cms";
 import Footer from "../../components/Footer";
 import Button from "../../components/Button";
 import Layout from "../../components/Layout";
+import UserStore from "../../stores/userStore";
 
-const About: FunctionComponent<RouteComponentProps> = ({ history }) => (
-  <Layout>
-    <div className="flex-container flex-container--no-padding flex-container--center about">
-      <Breadcrumb
-        crumbs={[
-          { text: "Home", url: "/" },
-          { text: "About the project", url: "" }
-        ]}
-      />
+interface IProps extends RouteComponentProps {
+  userStore?: UserStore;
+}
 
-      <div className="flex-col--12 about--container">
-        <h1 className="about--title">Who is Hearing Voices Network?</h1>
-        <p className="about--content">{cms("about.who")}</p>
+const About: FunctionComponent<IProps> = ({ userStore, history }) => {
+  if (!userStore) return null;
 
-        <h2 className="about--title">What is the project?</h2>
-        <p className="about--content">{cms("about.what")}</p>
-
-        <h3 className="about--title">Why develop the project?</h3>
-        <p className="about--content">{cms("about.why")}</p>
-      </div>
-    </div>
-    <Footer purple={true}>
-      <div className="flex-container flex-container--center flex-container--no-padding flex-container--align-center about--footer">
-        <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--8">
-          <h5 className="about--footer--title">{cms("about.footer.title")}</h5>
-          <p className="about--footer--content">
-            {cms("about.footer.content")}
-          </p>
+  return (
+    <Layout>
+      <div className="flex-container flex-container--no-padding flex-container--center about">
+        <div
+          className={cx("flex-col--12", {
+            "my-account--back--container": userStore.loggedIn
+          })}
+        >
+          {userStore.loggedIn ? (
+            <button
+              onClick={() => history.goBack()}
+              className="privacy-policy--back"
+            >
+              <FontAwesomeIcon icon="chevron-left" /> Back
+            </button>
+          ) : (
+            <Breadcrumb
+              crumbs={[
+                { text: "Home", url: "/" },
+                { text: "About the project", url: "" }
+              ]}
+            />
+          )}
         </div>
-        <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--4 about--footer--button">
-          <Button
-            text="Sign up and share"
-            onClick={() =>
-              history.push({
-                pathname: "/register"
-              })
-            }
-            twoCol={true}
-          />
+
+        <div className="flex-col--12 about--container">
+          <h1 className="about--title">Who is Hearing Voices Network?</h1>
+          <p className="about--content">{cms("about.who")}</p>
+
+          <h2 className="about--title">What is the project?</h2>
+          <p className="about--content">{cms("about.what")}</p>
+
+          <h3 className="about--title">Why develop the project?</h3>
+          <p className="about--content">{cms("about.why")}</p>
         </div>
       </div>
-    </Footer>
-  </Layout>
-);
+      <Footer purple={true}>
+        <div className="flex-container flex-container--center flex-container--no-padding flex-container--align-center about--footer">
+          <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--8">
+            <h5 className="about--footer--title">
+              {cms("about.footer.title")}
+            </h5>
+            <p className="about--footer--content">
+              {userStore.loggedIn
+                ? cms("about.footer-loggedin")
+                : cms("about.footer.content")}
+            </p>
+          </div>
+          <div className="flex-col--mobile--12 flex-col--tablet--12 flex-col--4 about--footer--button">
+            <Button
+              text={
+                userStore.loggedIn
+                  ? "Submit new experience"
+                  : "Sign up and share"
+              }
+              onClick={() =>
+                history.push({
+                  pathname: userStore.loggedIn
+                    ? "/submit-experience"
+                    : "/register"
+                })
+              }
+              twoCol={true}
+            />
+          </div>
+        </div>
+      </Footer>
+    </Layout>
+  );
+};
 
-export default withRouter(About);
+export default inject("userStore")(withRouter(About));
